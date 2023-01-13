b52453392b38ffa0e779fcd08313d3060ab8d335
Ewa Czajka <czajka.ewa.anna@gmail.com>
2023-01-13 23:13:19 +0100

    refactor auth errors messages

--- src/helpers/authErrorMsg.js
+++ src/helpers/authErrorMsg.js
@@ -1,27 +1,18 @@
 export const authErrorMsg = error => {
-    let message
     switch (true) {
         case error.includes('email-already-in-use'):
-            message = 'Account connected with this email already exists.'
-            break
+            return 'Account connected with this email already exists.'
         case error.includes('invalid-email'):
-            message = 'Email address is incorrect.'
-            break
+            return 'Email address is incorrect.'
         case error.includes('user-not-found'):
-            message = 'There is no account connected with this email.'
-            break
+            return 'There is no account connected with this email.'
         case error.includes('wrong-password'):
-            message = 'Incorrect password.'
-            break
+            return 'Incorrect password.'
         case error.includes('invalid-password'):
-            message = 'Password must have at least six characters.'
-            break
+            return 'Password must have at least six characters.'
         case error.includes('weak-password'):
-            message = 'Password must have at least six characters.'
-            break
+            return 'Password must have at least six characters.'
         default:
-            message = 'Please, enter a correct data.'
+            return 'Please, enter a correct data.'
     }
-
-    return message
 }
